ca25d31f53b4b8716c19f89a1dee871e80b42964
Tarek Schmeling <tarek.schmeling@schmeling-ol.de>
2025-05-14 15:35:54 +0200

    - add dataset transformations

--- load_datasets/CIFAR10.py
+++ load_datasets/CIFAR10.py
@@ -4,16 +4,22 @@ from torch.utils.data import DataLoader
 
 
 def cifar10(args, split):
-    transform = transforms.Compose([
+    train_transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Resize((args.img_size, args.img_size)),  # original: 32x32
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+    ])
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
     ])
     dl = None
     if split == 'train':
-        train_dataset = torchvision.datasets.CIFAR10(root='../datasets', train=True, transform=transform, download=True)
+        train_dataset = torchvision.datasets.CIFAR10(root='../datasets', train=True, transform=train_transform, download=True)
         dl = DataLoader(train_dataset, batch_size=args.train_batch_size, shuffle=True)
     elif split == 'test':
-        test_dataset = torchvision.datasets.CIFAR10(root='../datasets', train=False, transform=transform, download=True)
+        test_dataset = torchvision.datasets.CIFAR10(root='../datasets', train=False, transform=test_transform, download=True)
         dl = DataLoader(test_dataset, batch_size=args.test_batch_size, shuffle=False)
     return dl
 
